7052b9ac6a927d6a065f33df64986b4a65efe3f2
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2024-11-13 16:32:08 +0100

    ✨ Allow Env var interpolation

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,41 @@
 import { config } from "dotenv";
 import { z } from "zod";
 
-export const parseConfig = () => {
+export const parseConfig = (env = process.env) => {
+  const envVar = (target: z.ZodTypeAny = z.string()) =>
+    z
+      .string()
+      .transform((arg) => readEnvOrAlias(arg, env))
+      .pipe(target);
   const configSchema = z.object({
     // Potentiel API
-    API_URL: z.string().url(),
+    API_URL: envVar(z.string().url()),
     // OAuth configuration
-    ISSUER_URL: z.string().url(),
-    CLIENT_ID: z.string(),
-    CLIENT_SECRET: z.string(),
+    ISSUER_URL: envVar(z.string().url()),
+    CLIENT_ID: envVar(),
+    CLIENT_SECRET: envVar(),
     // S3 configuration
-    S3_ENDPOINT: z.string().url(),
-    S3_BUCKET: z.string(),
-    S3_REGION: z.string(),
-    S3_ACCESS_KEY: z.string(),
-    S3_SECRET_KEY: z.string(),
-    UPLOAD_FILE_PATH_TEMPLATE: z.string(),
-    DOWNLOAD_FOLDER: z.string(),
+    S3_ENDPOINT: envVar(z.string().url()),
+    S3_BUCKET: envVar(),
+    S3_REGION: envVar(),
+    S3_ACCESS_KEY: envVar(),
+    S3_SECRET_KEY: envVar(),
+    UPLOAD_FILE_PATH_TEMPLATE: envVar(),
+    DOWNLOAD_FOLDER: envVar(),
   });
   config();
-  return configSchema.parse(process.env);
+  return configSchema.parse(env);
+};
+
+/**
+ * Enables using aliases in env var.
+ * ie `MONGO_URL=$DATABASE_URL`
+ * will return the value of `env.DATABASE_URL`
+ */
+const readEnvOrAlias = (val: string, env: NodeJS.Dict<string>) => {
+  if (typeof val === "undefined") return;
+  // https://stackoverflow.com/questions/68970312/how-do-i-resolve-a-javascript-template-literal-variable-within-a-string-within-a
+  return val.replace(/\$\{(.*?)\}/g, (substring, key) =>
+    env[key] ? env[key] : substring
+  );
 };
